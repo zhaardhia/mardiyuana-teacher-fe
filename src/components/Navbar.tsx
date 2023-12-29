2ea92a31d661a3c6d64ea3d270291e8b5fdeb143
Fadli Rizaldy <fadli.rizaldy@@shopee.com>
2023-12-30 00:25:35 +0700

    feat: add profile page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,27 @@
-import React from 'react'
-import { Icon } from '@iconify/react'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from "react";
+import { Icon } from "@iconify/react";
+import Link from "next/link";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <nav className="fixed z-50 backdrop-blur-lg w-full bg-white">
       <div className="flex justify-between py-3 px-5 text-xl w-full mx-auto items-center">
         <Link href="/" className="flex gap-3">
-          <Image src={"/my_logo.png"} width={25} height={20} alt={'mardiyuana logo'} />
+          <Image src={"/my_logo.png"} width={25} height={20} alt={"mardiyuana logo"} />
           SMP Mardi Yuana
         </Link>
         <div className="flex items-center gap-3 p-2">
           <p className="text-sm">Welcome, Jamets</p>
-          <div className="w-[2rem] h-[2rem] rounded-full bg-slate-100"></div>
-          {/* <Link href="/" className={cn(
-            "hover:border-[1px] hover:bg-slate-50 border-slate-300 p-1 rounded-xl w-[30px] h-[30px] flex justify-center"
-          )}><Icon icon="mdi:creation-outline" /></Link> */}
-          {/* <Link href="/profile" className="hover:border-[1px] hover:bg-slate-50 border-slate-300 p-1 rounded-xl w-[30px] h-[30px] flex justify-center"><Icon icon="iconamoon:profile" /></Link> */}
-          
+          <div className="cursor-pointer" onClick={() => router.push(`/profile/eD98k-Lj23`)}>
+            <img src="/photo_profile.jpg" alt="" className="w-10 h-10 rounded-full object-cover" />
+          </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/pages/profile/[profileId].tsx
@@ -0,0 +1,69 @@
+import Layout from "@/components/Layout";
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <Layout>
+      <div className="flex justify-between items-center mb-8 w-[90%] mx-auto max-w-[1400px]">
+        <h1 className="text-2xl font-semibold">Profile</h1>
+        <p>Jumat, 3 Desember 1945</p>
+      </div>
+
+      <hr className="h-[2px] bg-[#AFAFAF]" />
+
+      <div className="my-5 w-[90%] mx-auto py-3 flex flex-col gap-14 max-w-[1400px]">
+        <div className="flex justify-center gap-10">
+          <img src="/photo_profile.jpg" alt="" className="w-72 h-72 rounded-full object-cover" />
+          <div className="flex flex-col justify-center gap-1">
+            <label htmlFor="" className="font-medium text-lg text-gray-700">
+              Ubah Gambar
+            </label>
+            <button className="py-1 px-4 text-lg rounded-[6px] text-white bg-green-600">Edit</button>
+          </div>
+        </div>
+        <div className="flex gap-10 justify-center">
+          <div className="flex flex-col gap-3 w-1/3">
+            <div>
+              <label htmlFor="" className="font-medium text-lg">
+                Name
+              </label>
+              <input
+                type="text"
+                className="rounded-[8px] border border-gray-400 bg-[#F9FAFB] focus:outline-primaryBtn px-3 w-full py-2"
+                value={"Fadli Rizaldy"}
+              />
+            </div>
+            <div>
+              <label htmlFor="" className="font-medium text-lg">
+                Kelas
+              </label>
+              <input
+                type="text"
+                className="rounded-[8px] border border-gray-400 bg-[#F9FAFB] focus:outline-primaryBtn px-3 w-full py-2"
+                value={"IX-A"}
+              />
+            </div>
+            <div>
+              <label htmlFor="" className="font-medium text-lg">
+                Wali Kelas
+              </label>
+              <input
+                type="text"
+                className="rounded-[8px] border border-gray-400 bg-[#F9FAFB] focus:outline-primaryBtn px-3 w-full py-2"
+                value={"Novaria"}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="" className="font-medium text-lg">
+              Ubah Password
+            </label>
+            <button className="py-1 px-4 text-lg rounded-[6px] text-white bg-green-600">Edit</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ProfilePage;
