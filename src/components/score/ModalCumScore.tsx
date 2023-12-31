200eb16cbd2ad8c4b6da3f53bede26fa22f787ec
Fadli Rizaldy <fadli.rizaldy@@shopee.com>
2023-12-31 14:19:03 +0700

    feat: fix score page table

---
+++ src/components/score/ModalCumScore.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+
+import { Icon } from "@iconify/react";
+
+type ModalCumPropsType = {
+  type: string;
+  subject: string;
+};
+
+const ModalCumScore = (props: ModalCumPropsType) => {
+  const { type, subject } = props;
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Icon icon="iconoir:question-mark-circle" className="text-sm cursor-pointer" />
+      </DialogTrigger>
+      <DialogContent className="bg-white w-1/2 px-10">
+        <DialogHeader className="flex flex-col items-center">
+          <DialogTitle className="text-2xl capitalize">Cummulative {type}</DialogTitle>
+          <DialogDescription className="text-xl font-normal capitalize">{subject}</DialogDescription>
+          <hr className="h-[2px] bg-[#AFAFAF]" />
+        </DialogHeader>
+        <Table className="bg-white rounded-xl">
+          <TableHeader>
+            <TableRow className="bg-slate-200">
+              <TableHead className="text-xl py-3 text-center">Quiz 1</TableHead>
+              <TableHead className="text-xl py-3 text-center">Quiz 2</TableHead>
+              <TableHead className="text-xl py-3 text-center">Quiz 3</TableHead>
+              <TableHead className="text-xl py-3 text-center">Quiz 4</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow className="group">
+              <TableCell className="text-xl border-b-2 border-slate-400 py-3 text-center">90</TableCell>
+              <TableCell className="text-xl border-b-2 border-slate-400 py-3 text-center">99</TableCell>
+              <TableCell className="text-xl border-b-2 border-slate-400 py-3 text-center">87</TableCell>
+              <TableCell className="text-xl border-b-2 border-slate-400 py-3 text-center">67</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default ModalCumScore;
--- src/pages/score/index.tsx
+++ src/pages/score/index.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@/components/Layout";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import Select, { ActionMeta } from "react-select";
 import { useRouter } from "next/router";
+import { Icon } from "@iconify/react";
+import ModalCumScore from "@/components/score/ModalCumScore";
+
+type Option = { value: string; label: string };
+
+const options = [
+  { value: "X - Semester 1", label: "X - Semester 1" },
+  { value: "X - Semester 2", label: "X - Semester 2" },
+  { value: "XI - Semester 1", label: "XI - Semester 1" },
+  { value: "XI - Semester 2", label: "XI - Semester 2" },
+  { value: "XII - Semester 1", label: "XII - Semester 1" },
+  { value: "XII - Semester 2", label: "XII - Semester 2" },
+];
 
 const ScorePage = () => {
   const router = useRouter();
+  const [selectedClass, setSelectedClass] = useState<Option>(options[0]);
+  const handleSelectClass = (option: Option | null, actionMeta: ActionMeta<Option>) => {
+    option && setSelectedClass(option);
+  };
+
   return (
     <Layout>
       <div className="flex justify-between items-center mb-8 w-[90%] mx-auto max-w-[1400px]">
@@ -14,35 +33,81 @@ const ScorePage = () => {
 
       <hr className="h-[2px] bg-[#AFAFAF]" />
 
-      <div className="w-[90%] mx-auto flex gap-14 items-center max-w-[1400px]">
-        <Table className="bg-white rounded-xl">
+      <div className="my-5 w-[90%] mx-auto py-3 flex gap-14 items-center max-w-[1400px]">
+        <Select
+          name="class"
+          className="basic-single w-[18%] rounded-xl"
+          value={selectedClass}
+          classNamePrefix="select"
+          isClearable={false}
+          isSearchable={false}
+          defaultValue={selectedClass}
+          options={options}
+          placeholder="Pilih Kelas"
+          onChange={handleSelectClass}
+        />
+      </div>
+
+      <div className="w-[90%] mx-auto flex gap-14 rounded-xl shadow-xl items-center max-w-[1400px]">
+        <Table className="bg-white rounded-xl ">
           <TableHeader>
             <TableRow>
-              <TableHead className="text-xl py-3 text-center">No</TableHead>
               <TableHead className="text-xl py-3 text-center">Mata Pelajaran</TableHead>
-              <TableHead className="text-xl py-3 text-center">Guru</TableHead>
-              <TableHead className="text-xl py-3 text-center">Action</TableHead>
+              <TableHead className="text-xl py-3 text-center">Cum. Assignment</TableHead>
+              <TableHead className="text-xl py-3 text-center">Cum. Quiz</TableHead>
+              <TableHead className="text-xl py-3 text-center">Mid Exam</TableHead>
+              <TableHead className="text-xl py-3 text-center">KKM</TableHead>
+              <TableHead className="text-xl py-3 text-center">Final Exam</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
             {[...Array(10)].map((_, idx) => {
               return (
-                <TableRow className="group" onClick={() => router.push(`/course/detail/${idx + 1}`)}>
-                  <TableCell className="text-lg py-3 text-center">{idx + 1}</TableCell>
-                  <TableCell className="text-lg py-3 text-center">Fisika</TableCell>
-                  <TableCell className="text-lg py-3 text-center">Novaria Kemmel S.Pd.</TableCell>
-                  <TableCell className="text-lg py-2 flex justify-center">
-                    <div className="w-24 flex justify-center">
-                      <button className="py-1 px-5 w-full rounded-[6px] text-white bg-green-600 font-medium relative hover:bg-green-700 hidden group-hover:block">
-                        Detail
-                      </button>
+                <TableRow className="group">
+                  <TableCell className="text-lg py-3 text-center">Seni Budaya</TableCell>
+                  <TableCell className="text-lg py-3 text-center">
+                    <div className="flex justify-center items-start">
+                      <p>90</p>
+                      <ModalCumScore type="Assignment" subject="Fisika" />
                     </div>
                   </TableCell>
+                  <TableCell className="text-lg py-3 text-center">
+                    <div className="flex justify-center items-start">
+                      <p>86</p>
+                      <ModalCumScore type="Quiz" subject="Olahraga" />
+                    </div>
+                  </TableCell>
+                  <TableCell className="text-lg py-3 text-center">76</TableCell>
+                  <TableCell className="text-lg py-3 text-center">70</TableCell>
+                  <TableCell className="text-lg py-3 text-center">80</TableCell>
                 </TableRow>
               );
             })}
           </TableBody>
         </Table>
+      </div>
+
+      <div className="w-[90%] my-5 mx-auto flex justify-end gap-14 items-center max-w-[1400px]">
+        <div className="w-1/4 bg-white rounded-[6px] shadow-xl border border-slate-400">
+          <section className="flex justify-between p-4">
+            <div className="flex flex-col gap-2">
+              <p>Sakit</p>
+              <p>Izin</p>
+              <p>Alpha</p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p>2</p>
+              <p>4</p>
+              <p>0</p>
+            </div>
+          </section>
+          <hr className="h-[2px] bg-[#AFAFAF]" />
+
+          <section className="flex justify-between px-4 py-2">
+            <p>Mean</p>
+            <p>87</p>
+          </section>
+        </div>
       </div>
     </Layout>
   );
